5969c3170dc996781ad87b62876ab0d038f066ae
Adarsh Dev Singh <jamwalansh987@gmail.com>
2025-05-25 01:00:06 +0530

    changed auth a bit

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,38 +1,58 @@
 const User = require('../models/User');
 const { StatusCodes } = require('http-status-codes');
-// const jwt = require('jsonwebtoken');
-const { BadRequestError, UnauthenticatedError } = require('../errors')
-const bycrypt = require('bcryptjs');
+const { BadRequestError, UnauthenticatedError } = require('../errors');
+
 const register = async (req, res) => {
-  // const {name , email,password} = req.body;
-console.log(req.body);
-// const namak = await bycrypt.genSalt(10);
-// const hashedPassword = await bycrypt.hash(password,namak);
-// const tempUser = {name , email , password:hashedPassword}
-  const user = await User.create({ ...req.body });
-  // const token = jwt.sign({userId:user._id,name:user.name},'jwtsecret',{expiresIn: '30d',})
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    throw new BadRequestError('Please provide name, email, and password');
+  }
+
+  const existingUser = await User.findOne({ email });
+  if (existingUser) {
+    throw new BadRequestError('Email already in use');
+  }
+
+  const user = await User.create({ name, email, password });
   const token = user.createJWT();
-  res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
-}
+
+  res.status(StatusCodes.CREATED).json({
+    user: { name: user.name },
+    token,
+  });
+};
+
 const login = async (req, res) => {
   const { email, password } = req.body;
-  console.log(req.body);
+
+
   if (!email || !password) {
-    throw new BadRequestError('Please provide email and passsword');
+    throw new BadRequestError('Please provide email and password');
   }
+
+
   const user = await User.findOne({ email });
   if (!user) {
-    throw new UnauthenticatedError("Credentials are not valid");
+    throw new UnauthenticatedError('Invalid credentials');
   }
-  const passwordSahiHa = await user.comparePassword(password);
-  if(!passwordSahiHa){
-    throw new UnauthenticatedError("Credentials are not valid");
+
+
+  const isCorrectPassword = await user.comparePassword(password);
+  if (!isCorrectPassword) {
+    throw new UnauthenticatedError('Invalid credentials');
   }
+
+
   const token = user.createJWT();
-  res.status(StatusCodes.OK).json({ user: { name: user.name }, token });
-}
+
+  res.status(StatusCodes.OK).json({
+    user: { name: user.name },
+    token,
+  });
+};
 
 module.exports = {
-  login,
   register,
-}
+  login,
+};
